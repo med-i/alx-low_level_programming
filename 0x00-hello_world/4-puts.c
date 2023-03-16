fa2999d44a4d27ac183c7b40a2e336e814d00fe1
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-16 14:41:53 +0100

    Add task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints '"Programming is like building a multilingual puzzle'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
