7ca0e41a9672ffd78f808574571a543b12ebe621
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-16 16:34:14 +0100

    Fix 3 task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful ";
+	char quote[] = "and that piece of art is useful\" ";
 	char author[] = "- Dora Korpar, 2015-10-19\n";
 
 	int length = sizeof(quote) - 1;
