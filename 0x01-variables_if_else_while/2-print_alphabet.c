08f1fa95ecea6b93d5eac5b75d9774c8e97905d8
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-18 00:45:32 +0100

    Add tasks 2 and 3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,6 +7,14 @@
  */
 int main(void)
 {
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(97 + i);
+	}
+
+	putchar('\n');
 
 	return (0);
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,6 +7,19 @@
  */
 int main(void)
 {
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(97 + i);
+	}
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(65 + i);
+	}
+
+	putchar('\n');
 
 	return (0);
 }
