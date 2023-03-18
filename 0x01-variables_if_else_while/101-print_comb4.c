8ef028077c5119b1b85938ab113c4aa33c7c73f1
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-18 02:21:52 +0100

    Add task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,6 +7,36 @@
  */
 int main(void)
 {
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i; j < 10; j++)
+		{
+			if (i == j)
+				continue;
+
+			for (k = j; k < 10; k++)
+			{
+				if (j == k)
+					continue;
+
+				putchar(48 + i);
+				putchar(48 + j);
+				putchar(48 + k);
+
+				if (i == 7 && j == 8 && k == 9)
+				break;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
 
 	return (0);
 }
