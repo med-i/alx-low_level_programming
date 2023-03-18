aacea73ea9ce371c38b357e02024c8aa0bf27ed3
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-18 03:39:06 +0100

    Add task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,6 +7,43 @@
  */
 int main(void)
 {
+	int i;
+	int j;
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			a = 48 + i / 10;
+			b = 48 + i % 10;
+			c = 48 + j / 10;
+			d = 48 + j % 10;
+
+			if (a == b && c == d )
+				continue;
+
+			if (a == c && b == d)
+				continue;
+
+			putchar(a);
+			putchar(b);
+			putchar(' ');
+			putchar(c);
+			putchar(d);
+
+			if(a == 57 && b == 56 && c == 57 && d == 57)
+				break;
+
+			putchar(',');	
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
 
 	return (0);
 }
