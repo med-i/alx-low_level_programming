b13892c2d0041ac496b0eee10e95329d1e0fb3e4
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-21 21:03:35 +0000

    Add tasks 7 and 8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,3 +1,20 @@
 #include "main.h"
 
+/**
+ * print_last_digit - Prints the last digit of a number.
+ * @num: The number to use.
+ *
+ * Return: The last digit of a number.
+ */
+int print_last_digit(int num)
+{
+	int last_digit = num % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar('0' + last_digit);
+
+	return (last_digit);
+}
 
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,3 +1,36 @@
 #include "main.h"
 
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer,
+ *		starting from 00:00 to 23:59.
+ */
+void jack_bauer(void)
+{
 
+	int min;
+	int hrs;
+	char h_tens;
+	char h_ones;
+	char m_tens;
+	char m_ones;
+
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		h_tens = '0' + hrs / 10;
+		h_ones = '0' + hrs % 10;
+
+		for (min = 0; min < 60; min++)
+		{
+			_putchar(h_tens);
+			_putchar(h_ones);
+			_putchar(':');
+
+			m_tens = '0' + min / 10;
+			m_ones = '0' + min % 10;
+
+			_putchar(m_tens);
+			_putchar(m_ones);
+			_putchar('\n');
+		}
+	}
+}
