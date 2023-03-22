789c712b344c9badf59aeb98be159b55cc3cc29d
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-22 02:29:33 +0000

    Add task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,3 +1,37 @@
 #include "main.h"
 
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int i;
+	long fn, fn1, fn2;
+
+	for (i = 2; i <= 50; i++)
+	{
+		if (i == 2)
+		{
+			fn = 1;
+			fn1 = 1;
+			fn2 = 0;
+		}
+		else
+		{
+			fn2 = fn1;
+			fn1 = fn;
+			fn = fn1 + fn2;
+		}
+
+		printf("%ld", fn);
+
+		if (i < 50)
+			printf(", ");
+	}
+
+	printf("\n");
+}
+
 
