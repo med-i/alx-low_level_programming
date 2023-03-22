b74a304c6e8b87e799e8ead59e30908991306d80
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-22 02:09:30 +0000

    Add task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,3 +1,25 @@
 #include "main.h"
 
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 1024; i > 0; i--)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
 
