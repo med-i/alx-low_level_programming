7a7a25086e3060c1e80943923048b31ce5affc4b
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-23 20:51:29 +0000

    Add task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character.
+ * @c: The character to check.
+ *
+ * Return: 1 if c is uppercase. 0 otherwise.
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
+
