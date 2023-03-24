2ccc519bdafda3030e3a755d34cde495c5652a52
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-24 02:19:28 +0000

    Add task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,44 @@
 #include "main.h"
 
 /**
- * 
+ * print_number - Prints an integer.
+ * @n: The number to print.
+ */
+void print_number(int n)
+{
+	int temp;
+	int div = 1;
+	int d;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	temp = n;
+
+	while (temp > 0)
+	{
+		temp /= 10;
+		div *= 10;
+	}
+
+	div /= 10;
+	temp = n;
+
+	while (temp > 0)
+	{
+		d = temp / div;
+		_putchar('0' + d);
+		temp %= div;
+		div /= 10;
+	}
+}
+
