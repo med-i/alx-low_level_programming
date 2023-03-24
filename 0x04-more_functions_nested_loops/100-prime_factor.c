3acdcd34e65518e9b9bfd0206b4cbcb49b7d81c1
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-24 01:33:39 +0000

    Add task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,32 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * 
+ * main - Entry point.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long n = 612852475143;
+	long i = 2;
+
+	while (1)
+	{
+		if (n % i != 0)
+		{
+			i++;
+
+			if (i == n)
+			{
+				printf("%ld\n", i);
+				break;
+			}
+
+			continue;
+		}
+
+		n /= i;
+	}
+
+	return (0);
+}
