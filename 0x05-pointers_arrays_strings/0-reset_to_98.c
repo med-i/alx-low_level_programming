0f2b9aafeda01b04bec969b73774368306d2a4b5
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-27 05:56:46 +0000

    Add tasks 1, 2, and 3

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the pointer to point to the value 98.
+ * @n: The pointer to use.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to be checked.
+ * Return: The length of s.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	return (i);
+}
+
