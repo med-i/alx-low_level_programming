68cffeece4c65c771c50f59a226141ae851b41f6
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-27 06:12:36 +0000

    Add tasks 3 and 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,19 @@
 #include "main.h"
 
 /**
- *
+ * print_rev - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+void print_rev(char *s)
+{
+	int i = strlen(s) - 1;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+
+	_putchar('\n');
+}
+
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,10 @@
 #include "main.h"
 
 /**
- *
+ * rev_string - Reverse a string.
+ * @s: The string to be reversed.
+ */
+void rev_string(char *s)
+{
+}
+
