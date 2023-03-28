9db21f9861069e2e78820f2bb92daeb1240edecf
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-28 03:06:02 +0000

    Add task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,26 @@
 #include "main.h"
 
 /**
+ * main - Entry point.
  *
+ * Return: 0.
+ */
+int main(void)
+{
+	char *pass;
+	int len;
+	int i;
+
+	srand(time(0));
+
+	len = rand() % 15 + 6;
+
+	pass = (char *)malloc(len * sizeof(char));
+
+	for (i = 0; i < len; i++)
+		pass[i] = (char)(rand() % 94 + 33);
+
+	printf("%s\n", pass);
+
+	return (0);
+}
