beb2e5d27fbc3f04a404dedf665c2caa1b062e87
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-30 02:07:58 +0000

    Add task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,36 @@
 #include "main.h"
 
 /**
+ * leet - Encodes a string into 1337.
+ * @str: The string to be encoded.
  *
- *
- *
- *
+ * Return: Pointer to the encoded string.
  */
-int main(void)
+char *leet(char *str)
 {
+	int i = 0;
+	int j = 0;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+
+	while (str[i] != '\0')
+	{
+		char c = str[i];
+
+		while (letters[j] != '\0')
+		{
+			if (c == letters[j])
+			{
+				str[i] = numbers[j];
+				break;
+			}
+
+			j++;
+		}
+
+		j = 0;
+		i++;
+	}
+
+	return (str);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,6 +11,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 int is_separator(char c);
+char *leet(char *str);
 char *rot13(char *str);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
