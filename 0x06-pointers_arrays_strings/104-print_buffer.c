0d5ee96ea35cc5bbcaccc5ca0b14feae6d49a957
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-31 02:36:53 +0000

    Add task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,29 +8,65 @@
 void print_buffer(char *b, int size)
 {
 	int i;
+	int count;
 	int len = 0;
+	int bytes = size < 10 ? size - 1 : 10;
 
-	while (b[len] < size)
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	while (len < size)
 	{
 		printf("%p: ", b + len);
 
-		for (i = len; i < len + 10; i++)
+		for (i = len, count = 0; i < len + bytes; i++)
 		{
 			printf("%02x", b[i]);
 
-			if (i > len && i % 2 != 0)
-			{
+			if (i % 2 != 0)
 				printf(" ");
-			}
+
+			count++;
 		}
 
-		for (i = len; i < len + 10; i++)
+		if (i == size - 1 && i % 2 != 0)
 		{
-			printf("%c", b[i]);
+			printf("00");
+
+			if (i % 2 != 0)
+				printf(" ");
+
+			count++;
+		}
+
+		while (count < 10)
+		{
+			printf("  ");
+
+			if (count % 2 != 0)
+				printf(" ");
+
+			count++;
+		}
+
+		for (i = len; i < len + bytes; i++)
+		{
+			char c = b[i];
+
+			if (c == '\0' || c == '\1' || c == '\2' || c == '\3' || c == '\4' || c == '\5' || c == '\6' || c == '\7' || c == '\n')
+				printf(".");
+			else
+				printf("%c", b[i]);
 		}
 
 		printf("\n");
 
-		len += 10;
+		if (size - len < 10)
+			bytes = size - len;
+
+		len += bytes;
 	}
 }
