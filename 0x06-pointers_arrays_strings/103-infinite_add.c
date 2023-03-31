b4d09bb4f5bce4419d22e876a519bbe2f5021e2d
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-03-31 00:01:13 +0000

    Add task 10

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * infinite_add - Adds two numbers.
+ * @n1: The first number.
+ * @n2: The second number.
+ * @r: The buffer.
+ * @size_r: The size of the buffer.
+ * 
+ * Return: Pointer to the result.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-
+	char res[]
 }
--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,12 +1,36 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * print_buffer - Prints a buffer.
+ * @b: The buffer to be printed.
+ * @size: The size to print.
  */
 void print_buffer(char *b, int size)
 {
+	int i;
+	int len = 0;
 
+	while (b[len] < size)
+	{
+		printf("%p: ", b + len);
+
+		for (i = len; i < len + 10; i++)
+		{
+			printf("%02x", b[i]);
+
+			if (i > len && i % 2 != 0)
+			{
+				printf(" ");
+			}
+		}
+
+		for (i = len; i < len + 10; i++)
+		{
+			printf("%c", b[i]);
+		}
+
+		printf("\n");
+
+		len += 10;
+	}
 }
