eb3e1cffcc5ec7b2ce7d621da6d2bbc43821bb8a
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-04-04 01:30:56 +0000

    Add task 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,10 +2,8 @@
 
 /**
  * set_string - Sets the value of a pointer to a char.
- * @s:
- * @to:
- *
- * Return:
+ * @s: The string to set.
+ * @to: The string to copy.
  */
 void set_string(char **s, char *to)
 {
