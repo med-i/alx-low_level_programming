5c926c955b3d0f12ad1837b10bf240e981fa89af
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-04-06 02:54:13 +0000

    Add task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
 /**
+ * compare - compares two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @idx1: index of s1.
+ * @idx2: index of s2.
+ *
+ * Return: 1 if s1 equals s2, 0 otherwise.
+ */
+int compare(char *s1, char *s2, int idx1, int idx2)
+{
+	if (s2[idx2] == '*')
+		idx2++;
+	else if (s1[idx1] != s2[idx2] && s2[idx2] != '*')
+		return (0);
+
+	idx1++;
+	idx2++;
+
+	if (!s1[idx1])
+		return (1);
+
+	return (compare(s1, s2, idx1, idx2));
+}
+
+/**
  * wildcmp - checks if two strings are considered identical.
  * @s1: first string.
  * @s2: second string.
@@ -9,5 +34,5 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	
+	return (compare(s1, s2, 0, 0));
 }
