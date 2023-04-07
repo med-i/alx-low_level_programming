1cae8c2c58738120ade4772655317cb77200c212
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-04-07 17:36:20 +0000

    Add task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point. This program prints its name.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
