364ab250c4cbfc1cd86ef81dc39a000ed0985f19
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-04-14 01:03:47 +0000

    Add task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr = malloc(nmemb * size);
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
 	return (ptr);
 }
