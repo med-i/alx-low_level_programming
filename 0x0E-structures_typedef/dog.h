4004fb5f12364d02df1976c1fb97bb6689867532
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-04-17 09:37:18 +0000

    Fix task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,6 +5,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * struct dog - a struct that represents a dog.
+ * @name: the name of the dog.
+ * @age: the age of the dog.
+ * @owner: the owner of the dog.
+ */
 struct dog
 {
 	char *name;
