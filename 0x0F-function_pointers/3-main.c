cceed366097cfa488dfbd63e50e6ecc32a04d579
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-04-20 00:37:43 +0000

    Add task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include "3-calc.h"
+
+/**
+ * main - performs simple arithmetic operations.
+ * @argc: the number of arguments.
+ * @argv: the array of arguments.
+ *
+ * Return:
+ * is succesful,
+ * if number of arguments is wrong,
+ * if the operator is wrong,
+ * if division by 0.
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2, res;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+
+	if (!op)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((!strcmp(op, "/") || !strcmp(op, "%")) && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	res = get_op_func(op)(num1, num2);
+	printf("%d\n", res);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addition.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: a + b;
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: a - b;
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: a * b;
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: a / b;
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: a % b;
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
