3bc7bf01101085d7d76ef860f52a85070fb93af4
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-04-27 18:47:05 +0000

    Add task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,14 @@
-#include "lists.h"
+#include <stdio.h>
+
+void before_main(void) __attribute__((constructor));
 
 /**
- * 
-*/
-
+ * before_main - prints a string before main runs
+ *
+ * Return: void
+ */
+void before_main(void)
 {
-	
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,9 +1,12 @@
 #include "lists.h"
 
 /**
- * 
-*/
-
+ * free_list - frees a list_t list
+ * @head: pointer to the head of the list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
 {
 	
 }
