14b911b4aaf946497d677d61109c18b37572a48d
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-04-27 13:19:24 +0000

    Add task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,9 +1,20 @@
 #include "lists.h"
 
 /**
- * 
-*/
-
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: the head of the list.
+ * @str: the string to duplicate.
+ *
+ * Return: the address of the new element, otherwise NULL.
+ */
+list_t *add_node(list_t **head, const char *str)
 {
-	
+	list_t *new = malloc(sizeof(list_t));
+
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
 }
