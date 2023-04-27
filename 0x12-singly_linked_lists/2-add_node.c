049152b36e70f1991e23437b1dd3d2b912b8c705
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-04-27 13:21:39 +0000

    Add task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,6 +11,9 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
 
+	if (!head || !new)
+		return (NULL);
+
 	new->str = strdup(str);
 	new->len = strlen(str);
 	new->next = *head;
