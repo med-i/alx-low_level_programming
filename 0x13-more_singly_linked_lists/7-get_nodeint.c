465732cbf8d00b91babbcbdfe569ac0fd82fdaab
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-04-30 14:22:46 +0100

    Add task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,6 +13,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *node;
 	unsigned int i = 0;
 
+	if (!head)
+		return (NULL);
+
 	while (head && i <= index)
 	{
 		if (index == i)
