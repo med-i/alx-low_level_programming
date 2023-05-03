c0adaea487bf13078682144447ee848361b5742a
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-05-03 21:52:22 +0100

    Add task 14

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -21,10 +21,11 @@ listint_t *find_listint_loop(listint_t *head)
 	{
 		slow = slow->next;
 
-		if (fast && fast->next)
-			fast = fast->next->next;
+		if (!fast || !fast->next)
+			continue;
+		fast = fast->next->next;
 
-		if (fast && slow == fast)
+		if (slow == fast)
 		{
 			slow = head;
 			while (slow != fast)
