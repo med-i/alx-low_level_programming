c5511ba27eed8cb0259382856eb1f03f32bc3146
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-05-04 03:01:28 +0100

    Add task 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,11 +8,21 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *previous = NULL;
+	listint_t *next = NULL;
 
-	if (!*head || !temp)
-	{
+	if (!head || !*head)
 		return (NULL);
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = previous;
+		previous = *head;
+		*head = next;
 	}
-	
+
+	*head = previous;
+
+	return (*head);
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,4 +8,38 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
+	listint_t *slow, *fast, *current;
+	int count = 0;
+
+	if (!h || !*h)
+		return (0);
+
+	slow = *h;
+	fast = *h;
+
+	while (slow)
+	{
+		current = slow;
+		slow = slow->next;
+		free(current);
+		count++;
+
+		if (fast && fast->next)
+			fast = fast->next->next;
+
+		if (fast && slow == fast)
+		{
+			while (slow != fast)
+			{
+				current = slow;
+				slow = slow->next;
+				free(current);
+				count++;
+			}
+		}
+	}
+
+	*h = NULL;
+
+	return (count);
 }
