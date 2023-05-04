2b1c30f223ef7fd413758da490785e9000d44899
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-05-04 23:49:52 +0100

    Add task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,10 +9,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int mask;
+
 	if (index >= sizeof(n) * 8)
 		return (-1);
 
-	unsigned long int mask = 1 << index;
-
+	mask = 1 << index;
 	return (n & mask ? 1 : 0);
 }
