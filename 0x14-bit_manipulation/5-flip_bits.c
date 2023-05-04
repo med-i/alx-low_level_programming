1df1f127d9358a309fe18cb8770c3357db19fde5
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-05-05 00:26:28 +0100

    Add task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,4 +9,15 @@
  * Return: number of bits to flip to get from n to m
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
-{}
+{
+	unsigned long int xor = n ^ m;
+	int count = 0;
+
+	while (xor > 0)
+	{
+		count += xor & 1;
+		xor >>= 1;
+	}
+
+	return (count);
+}
