7099968a909d00a93bce015b7d5c33339b1b82b2
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-05-04 23:18:40 +0100

    Add task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,4 +8,23 @@
  */
 void print_binary(unsigned long int n)
 {
+	int i = sizeof(n) * 8 - 1;
+	int has_leading_one = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	for (; i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+		{
+			_putchar('1');
+			has_leading_one = 1;
+		}
+		else if (has_leading_one)
+			_putchar('0');
+	}
 }
