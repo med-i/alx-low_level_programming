f8edacb4625788a32efcaf51b135b96c6608fdad
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-05-08 14:17:59 +0100

    Add task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,18 +9,56 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	/* 	int fd;
+		char buf[1024];
+		int read_bytes = 0;
+		int count = 0;
+
+		fd = open(filename, O_RDONLY);
+		if (fd == -1)
+			return (0);
+
+		read_bytes = read(fd, buf, letters);
+		count = write(STDOUT_FILENO, buf, read_bytes);
+
+		close(fd);
+		return (count == -1 ? 0 : count); */
+
 	int fd;
-	char buf[1024];
-	int read_bytes = 0;
-	int count = 0;
+	ssize_t n_read, n_written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	read_bytes = read(fd, buf, letters);
-	count = write(STDOUT_FILENO, buf, read_bytes);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	n_written = write(STDOUT_FILENO, buffer, n_read);
+	if (n_written == -1 || n_written != n_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
 	close(fd);
-	return (count == -1 ? 0 : count);
+	return (n_written);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,7 @@
 
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
