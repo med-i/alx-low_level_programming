b31ae28d8ee9befd47044b68c1ea8c5690123388
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-05-08 15:51:31 +0100

    Add task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,8 +16,11 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1 || !text_content)
+	if (fd == -1)
 		return (-1);
+
+	if (!text_content)
+		return (1);
 
 	w_bytes = write(fd, text_content, strlen(text_content));
 	if (w_bytes == -1)
