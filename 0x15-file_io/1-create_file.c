8727db893b85bfdafef3cd6c967e09f1d9c9df62
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-05-08 15:20:27 +0100

    Add task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,11 +19,13 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	if (!text_content)
+		return (1);
+
 	w_bytes = write(fd, text_content, strlen(text_content));
 	if (w_bytes == -1)
 		return (-1);
 
 	close(fd);
-
 	return (1);
 }
