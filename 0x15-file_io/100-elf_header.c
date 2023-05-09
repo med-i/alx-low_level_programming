3cf0016a73bbde4c018a6878d2555932e89a491e
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-05-09 02:55:51 +0100

    Add task 3

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,2 +1,43 @@
 #include "main.h"
 
+int is_elf_file(int fd)
+{
+	char elf_magic_num[] = {0x7F, 'E', 'L', 'F'};
+	char file_magic_num[4];
+	int i;
+
+	read(fd, file_magic_num, 4);
+
+	for (i = 0; i < 4; i++)
+		if (file_magic_num[i] != elf_magic_num[i])
+			return (0);
+
+	return (1);
+}
+
+void print_magic_number()
+{
+}
+
+int main(int ac, char **av)
+{
+	int fd;
+
+	if (ac != 2)
+	{
+		dprintf(2, "Usage: elf_heade elf_filename\n");
+		exit(98);
+	}
+
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s", av[1]);
+		exit(98);
+	}
+
+	printf(is_elf_file(fd) ? "Is ELF file\n" : "Is not\n");
+
+	close(fd);
+	return (0);
+}
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * handle_error - handles errors
+ * print_error_and_exit - handles errors
  * @code: error code
  * @arg: argument to print
  */
-void handle_error(int code, void *arg)
+void print_error_and_exit(int code, void *arg)
 {
 	switch (code)
 	{
@@ -13,17 +13,49 @@ void handle_error(int code, void *arg)
 		dprintf(2, "Usage: cp file_from file_to\n");
 		break;
 	case 98:
-		dprintf(2, "Error: Can't read from file %s", (char *)arg);
+		dprintf(2, "Error: Can't read from file %s\n", (char *)arg);
 		break;
 	case 99:
-		dprintf(2, "Error: Can't write to %s", (char *)arg);
+		dprintf(2, "Error: Can't write to %s\n", (char *)arg);
 		break;
 	case 100:
-		dprintf(2, "Error: Can't close fd %d", *((int *)arg));
+		dprintf(2, "Error: Can't close fd %d\n", *((int *)arg));
 		break;
 	}
 
 	exit(code);
+}
+
+/**
+ * open_src_file - opens a file to read
+ * @filename: name of the file to open
+ *
+ * Return: file descriptor
+ */
+int open_src_file(char *filename)
+{
+	int fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		print_error_and_exit(98, filename);
+
+	return (fd);
+}
+
+/**
+ * open_des_file - opens a file to write
+ * @filename: name of the file to open
+ *
+ * Return: file descriptor
+ */
+int open_des_file(char *filename)
+{
+	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	if (fd == -1)
+		print_error_and_exit(99, filename);
+
+	return (fd);
 }
 
 /**
@@ -44,40 +76,35 @@ int main(int ac, char **av)
 	char buffer[1024];
 
 	if (ac != 3)
-		handle_error(97, NULL);
-	if (!av[1])
-		handle_error(98, av[1]);
-	if (!av[2])
-		handle_error(99, av[2]);
-	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
-		handle_error(98, av[1]);
-	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		if (close(fd_from) == -1)
-			handle_error(100, &fd_from);
-		handle_error(99, av[2]);
-	}
+		print_error_and_exit(97, NULL);
+
+	fd_from = open_src_file(av[1]);
+	fd_to = open_des_file(av[2]);
 
 	while ((r_bytes = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
 		w_bytes = write(fd_to, buffer, r_bytes);
+
 		if (w_bytes == -1 || w_bytes != r_bytes)
 		{
 			if (close(fd_from) == -1)
-				handle_error(100, &fd_from);
+				print_error_and_exit(100, &fd_from);
+
 			if (close(fd_to) == -1)
-				handle_error(100, &fd_to);
-			handle_error(99, av[2]);
+				print_error_and_exit(100, &fd_to);
+
+			print_error_and_exit(99, av[2]);
 		}
 	}
 
-	if (close(fd_from) == -1)
-		handle_error(100, &fd_from);
-	if (close(fd_to) == -1)
-		handle_error(100, &fd_to);
 	if (r_bytes == -1)
-		handle_error(98, av[1]);
+		print_error_and_exit(98, av[1]);
+
+	if (close(fd_from) == -1)
+		print_error_and_exit(100, &fd_from);
+
+	if (close(fd_to) == -1)
+		print_error_and_exit(100, &fd_to);
+
 	return (0);
 }
