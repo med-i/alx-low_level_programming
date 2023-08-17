196aa595838d0e3a5db7d3706a142425c7446c49
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-17 20:32:09 +0100

    Add task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,4 +2,17 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
+	size_t size = 0;
+
+	if (!h)
+		return (size);
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		size++;
+		h = h->next;
+	}
+
+	return (size);
 }
