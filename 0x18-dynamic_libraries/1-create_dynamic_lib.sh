91be01210113e1d34fe3d59655bfc2a1b2cba5ed
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-21 15:54:56 +0100

    Add task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,8 +1,4 @@
-## Resources
+#!/bin/bash
 
-**Read or watch**:
-
-- [What is difference between Dynamic and Static library (Static and Dynamic linking)](https://intranet.alxswe.com/rltoken/XLLmLISlteUIxrLzNdm3_Q "What is difference between Dynamic and Static library (Static and Dynamic linking)")
-- [create dynamic libraries on Linux](https://intranet.alxswe.com/rltoken/JEqzgE_pPe48rvbspGL-2g "create dynamic libraries on Linux")
-
-## Learning Objectives
+gcc -c -fPIC *.c
+gcc -shared -o liball.so *.o
