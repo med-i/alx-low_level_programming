f19e25ff95f873214b80c7447706d41e485fafaf
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-22 03:13:07 +0100

    Add task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/med-i/alx-low_level_programming/blob/main/0x18-dynamic_libraries/rand.so
+wget -P /tmp https://github.com/med-i/alx-low_level_programming/raw/main/0x18-dynamic_libraries/rand.so
 LD_PRELOAD=/tmp/rand.so
