2d0c06b2110930b5955b445af94586d988fb2515
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-25 18:37:11 +0100

    Fix task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/med-i/alx-low_level_programming/raw/main/0x18-dynamic_libraries/injection.so
-LD_PRELOAD=/tmp/injection.so
+export LD_PRELOAD=$LD_PRELOAD:/tmp/injection.so
--- 0x18-dynamic_libraries/injection.c
+++ 0x18-dynamic_libraries/injection.c
@@ -1,3 +1,6 @@
+#include <unistd.h>
+#include <string.h>
+
 int rand()
 {
 	static int ct = -1;
