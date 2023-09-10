5bc42f9776c2b69306b23098a7dda55585acdd65
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-09-10 22:38:30 +0100

    Fix task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,7 +10,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node, *temp;
+	hash_node_t *node;
 	unsigned long index;
 
 	if (!ht || !key || !*key)
