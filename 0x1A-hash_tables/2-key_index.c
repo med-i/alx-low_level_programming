8b1663fc82667c7edd81cf8bbcccabf1c86afd3b
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-09-10 18:16:59 +0100

    Include main.c files

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,6 @@
+#include "hash_tables.h"
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	
+}
---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,6 @@
+#include "hash_tables.h"
+
+void hash_table_print(const hash_table_t *ht)
+{
+	
+}
