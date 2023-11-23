329d3a90f57be1cd3de20ccdd9cc3fcd605f9831
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-11-23 19:02:57 +0100

    Fix task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,19 +6,22 @@ This module defines the island_perimeter method.
 
 def island_perimeter(grid):
     """Returns the perimeter of the island described in grid."""
+    rows = len(grid)
+    cols = len(grid[0]) if rows > 0 else 0
     perimiter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+
+    for i in range(rows):
+        for j in range(cols):
             if grid[i][j] == 0:
                 continue
 
-            if not grid[i][j - 1]:
+            if i == 0 or not grid[i - 1][j]:
                 perimiter += 1
-            if not grid[i][j + 1]:
+            if i == rows - 1 or not grid[i + 1][j]:
                 perimiter += 1
-            if not grid[i - 1][j]:
+            if j == 0 or not grid[i][j - 1]:
                 perimiter += 1
-            if not grid[i + 1][j]:
+            if j == cols - 1 or not grid[i][j + 1]:
                 perimiter += 1
 
     return perimiter
