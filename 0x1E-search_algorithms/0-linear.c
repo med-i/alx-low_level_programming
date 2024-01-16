44aeb172c0d06237aa459f2c10f0e7b9026d4f92
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-01-16 13:01:36 +0100

    Add task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,14 +1,29 @@
 #include "search_algos.h"
 
 /**
- * linear_search - 
- * @array:
- * @size:
- * @value:
- * 
- * Return: 
-*/
+ * linear_search - searches for a value in an array of integers using
+ * the Linear search algorithm.
+ * @array: a pointer to the first element of the array to search in.
+ * @size: is the number of elements in array.
+ * @value: is the value to search for.
+ *
+ * Return: the first index where value is located, otherwise -1.
+ */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i;
+	int index = -1;
 
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+		{
+			index = i;
+			break;
+		}
+	}
+
+	return (index);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,14 +1,14 @@
 #include "search_algos.h"
 
 /**
- * binary_search - 
- * @array:
- * @size:
- * @value:
- * 
- * Return: 
-*/
+ * binary_search - searches for a value in a sorted array of integers using
+ * the Binary search algorithm.
+ * @array: a pointer to the first element of the array to search in.
+ * @size: is the number of elements in array.
+ * @value: is the value to search for.
+ *
+ * Return: the first index where value is located, otherwise -1.
+ */
 int binary_search(int *array, size_t size, int value)
 {
-    
 }
