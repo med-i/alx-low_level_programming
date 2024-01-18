7c5d0c445bf2514e47c824aa02f0124e80321924
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-01-18 12:00:42 +0100

    Handle NULL array

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,6 +14,9 @@ int linear_search(int *array, size_t size, int value)
 	size_t i;
 	int index = -1;
 
+	if (!array)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
